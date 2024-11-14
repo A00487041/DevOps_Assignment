9c20aab9d0e38df001d4236cb6a380ebfe18eb24
Mohammed Abdul Thoufiq <mohammed.abdul.thoufiq.-@smu.ca>
2024-11-14 15:10:02 -0400

    Delete dev directory

--- dev/src/AzureMvcApp-dev/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AzureMvcApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
